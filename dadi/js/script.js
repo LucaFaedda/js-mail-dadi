73bf69f5600e7a271423a4d9e4804a62ea9d0f4f
LucaFaedda <lucakarl91@gmail.com>
2022-11-30 16:50:27 +0100

    aggiunto per prova bottone per lancio casuale

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -7,14 +7,22 @@ let num_pc = Math.floor(Math.random()* 6) + 1
 
 console.log(`numero:giocatore: ${num_pc}`)
 
+const play_game = document.getElementById('premi')
 
-if(num_player ==  num_pc){
-    console.log("Rilancia")
-}
-else if(num_player > num_pc){
-    console.log("Giocatore HAI vinto")
-}
-else {
-    console.log("Giocatore HAI perso")
-}
+play_game.addEventListener('click', function(){
+
+    if(num_player ==  num_pc){
+        console.log("Rilancia")
+        alert('Rilancia')
+    }
+    else if(num_player > num_pc){
+        console.log("Giocatore HAI vinto")
+        alert('Hai vinto')
+    }
+    else {
+        console.log("Giocatore HAI perso")
+        alert('Hai perso')
+    }
+
+})    
 
